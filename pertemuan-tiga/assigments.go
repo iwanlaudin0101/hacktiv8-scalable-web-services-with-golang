eb219c8898c8e8b04090626558c3f78f50236356
iwanlaudin0101 <iwanlaudin0101@gmail.com>
2023-03-13 17:19:27 +0700

    assignment tiga selesai

--- pertemuan-tiga/assigments.go
+++ pertemuan-tiga/assigments.go
@@ -5,12 +5,21 @@ import (
 )
 
 func main() {
-	
+
 	tokenize := func (str string) {
+		charCounter := make(map[string]int)
+
 		for _, char := range str {
 			fmt.Println(string(char))
+
+			if val, key := charCounter[string(char)]; key {
+				charCounter[string(char)] = val + 1
+			} else {
+				charCounter[string(char)] = 1		
+			}
 		}
-		// return ""
+
+		fmt.Println(charCounter)
 	}
 
 	tokenize("selamat malam")
